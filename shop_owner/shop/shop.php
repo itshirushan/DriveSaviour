69a978d87c6617bcc6af1e612f0c7eae5dd0212c
Abhitha Vishvajith <162031145+AbhithaVish@users.noreply.github.com>
2024-08-31 08:30:46 +0530

    shop page (#4)

--- shop_owner/shop/shop.php
+++ shop_owner/shop/shop.php
@@ -6,14 +6,11 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shop</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <button>
-        <a href="../shop/shop.php">
-            shops
-        </a>
-    </button>
+    
 </body>
 </html>
---
+++ vehicle_owner/shop/shop.php
@@ -0,0 +1,49 @@
+<?php
+    require '../navbar/nav.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shop</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container-shops">
+        <div class="topic">
+             <h1>Shop</h1><br>
+
+        </div>
+        
+
+        <div class="title">
+            <div class="title-text">
+                <p>Explore a wide network of registered automotive shops through our Shop page, where quality and convenience meet. Whether you’re looking for a trusted mechanic, specialized repair services, or parts and accessories, DriveSaviour connects you with top-rated shops in your area. Each shop profile provides detailed information, including services offered, customer reviews, and contact details, helping you make informed decisions about your vehicle’s care.</p>
+                <button class="browse">
+                    <a href="../home/home.php">Browse</a>
+                </button>
+            </div>
+            <div class="title-image">
+                <img src="../../img/about.png" alt="Mechanic and Car">
+            </div>
+        </div>
+
+        <div class="carousel">
+            <button class="carousel-btn prev">&#10094;</button>
+            <div class="carousel-items">
+                <div class="carousel-item">1</div>
+                <div class="carousel-item">2</div>
+                <div class="carousel-item">3</div>
+                <div class="carousel-item">4</div>
+                <div class="carousel-item">5</div>
+                <div class="carousel-item">6</div>
+            </div>
+            <button class="carousel-btn next">&#10095;</button>
+        </div>
+    </div>
+
+    <script src="script.js"></script>
+</body>
+</html>
+
