7c0a615ac5179700297c1f39efbb165dc56497a5
abhitha@AV <abhithavishvajith@gmail.com>
2024-09-14 18:36:41 +0530

    Shop Owner Add Shop Upper Comma Fixed

--- shop_owner/shop/shop_manage.php
+++ shop_owner/shop/shop_manage.php
@@ -4,7 +4,7 @@ include_once('../../connection.php');
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $shop_id = $_POST['id'];
-    $shop_name = $_POST['shop_name'];
+    $shop_name = mysqli_real_escape_string($conn, $_POST['shop_name']);
     $email = $_POST['email'];
     $number = $_POST['number'];
     $address = $_POST['address'];
