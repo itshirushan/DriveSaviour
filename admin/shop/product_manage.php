d136a6bc5bea4e3a38b73de047482133a160c6cc
Ramitha Heshan <st20286184@outlook.cardiffmet.ac.uk>
2024-10-24 23:26:14 +0530

    admin shop adding and product adding feature

---
+++ admin/shop/product_manage.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+include_once('../../connection.php');
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $product_id = $_POST['id'];
+    $shop_id = $_POST['shop_id'];
+    $product_name = mysqli_real_escape_string($conn, $_POST['product_name']);
+    $quantity_available = (int) $_POST['quantity_available'];
+    $price = (float) $_POST['price'];
+
+    if ($_POST['action'] == 'edit') {
+        // Update query
+        $sql = "UPDATE products SET product_name='$product_name', quantity_available='$quantity_available', price='$price' WHERE id='$product_id'";
+        if (mysqli_query($conn, $sql)) {
+            header("Location: products.php?shop_id=" . $shop_id . "&message=edit");
+            exit;
+        } else {
+            $error = mysqli_error($conn);
+            header("Location: products.php?shop_id=" . $shop_id . "&message=error&error=" . urlencode($error));
+            exit;
+        }
+    } elseif ($_POST['action'] == 'delete') {
+        // Delete query
+        $sql = "DELETE FROM products WHERE id='$product_id'";
+        if (mysqli_query($conn, $sql)) {
+            header("Location: products.php?shop_id=" . $shop_id . "&message=delete");
+            exit;
+        } else {
+            $error = mysqli_error($conn);
+            header("Location: products.php?shop_id=" . $shop_id . "&message=error&error=" . urlencode($error));
+            exit;
+        }
+    }
+} else {
+    header("Location: products.php");
+    exit;
+}
+?>
